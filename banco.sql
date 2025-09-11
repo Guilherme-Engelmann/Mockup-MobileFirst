8b1e11e693f83ee1d878f1ef70bed2f36dcc5ec3
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-09-11 10:02:02 -0300

    Update - banco.sql

--- banco.sql
+++ banco.sql
@@ -1,49 +1,40 @@
-CREATE DATABASE ferrovia;
+CREATE DATABASE tracktrain;
 
-USE ferrovia;
+USE tracktrain;
 
-CREATE TABLE PerfilUsuario (
-    idPerfil INT AUTO_INCREMENT PRIMARY KEY,
-    nomePerfil VARCHAR(50) NOT NULL
-);
 
-CREATE TABLE Usuario (
-    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    tipoUsuario VARCHAR(50),
-    idPerfil INT,
-    FOREIGN KEY (idPerfil) REFERENCES PerfilUsuario(idPerfil)
+CREATE TABLE Manutencoes (
+    idManutencoes INT AUTO_INCREMENT PRIMARY KEY,
+    idTrem INT NOT NULL,
+    tipoManutencoes VARCHAR(50) NOT NULL,
+    descricaoManutencoes VARCHAR(255) NOT NULL,
+    data_agendada DATE NOT NULL,
+    data_conclusao DATE NOT NULL,
+    statusManutencoes VARCHAR(50) NOT NULL,
+    custoManutencoes DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (idTrem) REFERENCES Trem(idTrem)
 );
 
 CREATE TABLE Trem (
     idTrem INT AUTO_INCREMENT PRIMARY KEY,
-    identificacao VARCHAR(50) NOT NULL,
-    modelo VARCHAR(50),
-    capacidade INT
+    numero_serie INT NOT NULL,
+    modeloTrem VARCHAR(75) NOT NULL,
+    data_fabricacao DATE NOT NULL,
+    capacidade_passageiros INT NOT NULL,
+    capacidade_carga  VARCHAR(120) NOT NULL,
+    status_operacional  VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE DadoSensor (
-    idDado INT AUTO_INCREMENT PRIMARY KEY,
-    idTrem INT,
-    tipoSensor VARCHAR(50),
-    valorSensor FLOAT,
-    dataHora DATETIME,
-    FOREIGN KEY (idTrem) REFERENCES Trem(idTrem)
-);
-
-CREATE TABLE Rota (
-    idRota INT AUTO_INCREMENT PRIMARY KEY,
-    nomeRota VARCHAR(100),
-    demandaHistorica INT,
-    demandaAtual INT
-);
-
-CREATE TABLE Segmento (
-    idSegmento INT AUTO_INCREMENT PRIMARY KEY,
-    idRota INT,
-    idEstacaoInicio INT,
-    idEstacaoFim INT,
-    ordem INT,
+CREATE TABLE Viagens (
+    idViagem INT AUTO_INCREMENT PRIMARY KEY,
+    idTrem INT NOT NULL,
+    idRota INT NOT NULL,
+    horario_partida_previsto TIME NOT NULL,
+    horario_chegada_previsto TIME NOT NULL,
+    horario_partida_real TIME NOT NULL,
+    horario_chegada_real TIME NOT NULL,
+    statusViagens VARCHAR(50) NOT NULL,
+    FOREIGN KEY (idTrem) REFERENCES Trem(idTrem),
     FOREIGN KEY (idRota) REFERENCES Rota(idRota)
 );
+
