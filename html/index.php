08d2efd2b3df9af5e4a383be16659f9c84d9da8e
Guilherme Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-09-18 08:32:50 -0300

    add

--- html/index.php
+++ html/index.php
@@ -1,47 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Tela de Login</title>
-
-  <script src="validador.js"></script>
-
-  <link rel="stylesheet" href="style.css" />
-
+  <link rel="stylesheet" href="../css/style.css" />
 </head>
 <body>
 
-  <div class="container">
-    <div class="login-box">
-      <div class="avatar">
-        <img src="https://i.pinimg.com/736x/cf/3c/82/cf3c827e94daede62d82c30f28f5aab2.jpg" alt="Avatar">
+ 
+
+    <div class="phone-content">
+      <div class="header">
+        <i class="fas fa-arrow-left back-icon"></i>
+        <div class="icon-title">
+          <i class="fas fa-clipboard-list header-icon"></i>
+          <h2>Tela de login</h2>
+        </div>
       </div>
-      <div class="login-form">
-        <h2>Login</h2>
-        <form method="POST" action="login.php">
+
+      <div class="login-box">
+        <?php if (isset($_GET['message'])): ?>
+          <p style="color: green;"><?php echo htmlspecialchars($_GET['message']); ?></p>
+        <?php endif; ?>
+        <?php if (isset($_GET['error'])): ?>
+          <p style="color: red;"><?php echo htmlspecialchars($_GET['error']); ?></p>
+        <?php endif; ?>
+        <div class="avatar">
+          <img src="../imagens/perfil.png" alt="Avatar">
+        </div>
+
+        <form class="login-form" action="dashboard3.php" method="POST" id="meuFormulario">
+          <h2>Login</h2>
+
           <div class="input-group">
             <span class="icon">📧</span>
-            <input type="text" name="username" placeholder="Usuário ou e-mail" required />
+            <input type="email" placeholder="E-mail" name="email" required />
           </div>
 
           <div class="input-group">
-            <span class="icon">🔒</span>
-            <input type="password" name="password" placeholder="Senha..." required />
+            <input type="password" placeholder="Senha..." name="senha" id="senha" required />
+            <div class="error" id="erroSenha"></div>
           </div>
 
           <div class="options">
-            <label><input type="checkbox" name="remember" checked /> Mantenha-me Conectado</label>
+            <label><input type="checkbox" checked /> Mantenha-me Conectado</label>
             <a href="esqueceuasenha.php">Esqueceu a senha?</a>
           </div>
 
           <button type="submit" class="btn-login">Login</button>
-
+          <p class="register">Não tem uma conta ainda? <a href="inscreverse.php">Inscrever-se</a></p>
         </form>
-
-        <p class="register">Não tem uma conta ainda? <a href="inscreverse.php">Inscrever-se</a></p>
       </div>
     </div>
   </div>
+
+
+
 </body>
 </html>
