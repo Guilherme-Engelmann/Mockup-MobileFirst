420af9dff90552422dbc3cc146e61a580ba8638b
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-06 08:48:46 -0300

    update - style

--- html/esqueceuasenha.php
+++ html/esqueceuasenha.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['reset'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esqueceu a Senha</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/senha.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
