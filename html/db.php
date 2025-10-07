9e5e77dec67283347c6e6ee7a4a9d35aa33d7e6f
Allyson Schaedler Brinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-07 09:07:51 -0300

    Update db.php

--- html/db.php
+++ html/db.php
@@ -6,8 +6,8 @@ $password = "root";
 $dbname = "tracktrain";
 
 $mysqli = new mysqli($servername, $username, $password, $dbname);
-if($mysqli->connect_errno){
-    die("Erro de conexão: " . $mysqli->connect_error);
+if($mysqli->erro){
+    die("Erro de conexão: " . $mysqli->error);
 }
 
 ?>
