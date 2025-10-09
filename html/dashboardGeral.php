e2502c7dcea9ab05923a7f241410b31eb89c0ff6
BrinkerhoffDevCode <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-10-09 08:37:14 -0300

    update - style

--- html/dashboardGeral.php
+++ html/dashboardGeral.php
@@ -22,7 +22,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mockup Trem</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
