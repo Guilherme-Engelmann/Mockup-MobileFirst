992a90dec3f9261684df5ae3a031483590eac082
Polzomanga <victor_polzin@estudante.sesisenai.org.br>
2025-10-28 11:54:52 -0300

    add

--- html/create.php
+++ html/create.php
@@ -1,31 +1,31 @@
 <?php
 <?php
-// Conexão com o banco de dados
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "seu_banco"; // Altere para o nome do seu banco
+$dbname = "seu_banco";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica conexão
+
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Recebe dados do formulário
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login = $_POST['login'];
     $senha = $_POST['senha'];
 
-    // Validação simples
+  
     if (empty($login) || empty($senha)) {
         echo "Preencha todos os campos!";
     } else {
-        // Hash da senha
+       
         $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-        // Insere no banco
+        
         $sql = "INSERT INTO usuarios (login, senha) VALUES (?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ss", $login, $senha_hash);
