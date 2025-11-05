ace82d5885a333f6fcc0ee3c34f6d68ef0fd1247
Guilherme_Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-11-05 09:01:52 -0300

    ADD

--- html/db.php
+++ html/db.php
@@ -5,13 +5,11 @@ $username = "root";
 $password = "root";
 $dbname = "tracktrain";
 
- try {
-        $conn = new PDO("mysql: host=$servername;dbname=$dbname", $username, $password);
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e){
-        echo "Conexão falhou, erro: " . $e->getMessage();
-    }
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
 
 
